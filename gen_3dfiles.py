09c5d0dee8e1bf9907e6560e69926699b3c3b5ea
Mariusz Ryndzionek <mryndzionek@gmail.com>
2023-01-21 15:07:14 +0100

    Added error checking and adjusted the runner config

--- gen_3dfiles.py
+++ gen_3dfiles.py
@@ -8,11 +8,14 @@ from progress.bar import Bar
 def process(fn):
     i_file = os.path.basename(fn)
     o_file = os.path.splitext(i_file)[0]
-    subprocess.run(["./cq-cli/cq-cli", "--codec", "stl", "--infile", "keyboard.py",
-                    "--outfile", os.path.join("output", o_file + ".stl"),
-                    "--params", "i:{}".format(fn)],
-                   stdout=subprocess.DEVNULL,
-                   stderr=subprocess.DEVNULL)
+
+    ret = subprocess.run(["./cq-cli/cq-cli", "--codec", "stl", "--infile", "keyboard.py",
+                                 "--outfile", os.path.join("output",
+                                                           o_file + ".stl"),
+                                 "--params", "i:{}".format(fn)],
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+    assert(ret.returncode == 0)
     return o_file
 
 
